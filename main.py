5b6fca85901ff3a8323e8735a9ae4996083d0333
Mayboyzz <mayboyz99@gmail.com>
2020-07-04 22:15:41 -0500

    need to work on tkinter

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello world")
+import requests
+
+def get_weather(city):
+    weather_key = '1db1ab9a24c897ed0c0ebd6d3c726fb1'
+    url = "https://api.openweathermap.org/data/2.5/weather"
+    params = {'APPID': weather_key, 'q': city, 'units': 'imperial'}
+
+    response = requests.get(url, params=params)
+    print(response.json())
+
+get_weather('Sulphur')
